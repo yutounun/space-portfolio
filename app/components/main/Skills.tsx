ba1d76612cd5b293e59153435741a08711bb0254
yutounun <geek.yuto@gmail.com>
2024-09-21 13:26:29 -0700

    update skills

--- app/components/main/Skills.tsx
+++ app/components/main/Skills.tsx
@@ -1,5 +1,6 @@
 import {
   Backend_skill,
+  Css_skill,
   Frontend_skill,
   Full_stack,
   Other_skill,
@@ -18,7 +19,7 @@ const Skills = () => {
     >
       <SkillText />
 
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+      {/* <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Skill_data.map((image, index) => (
           <SkillDataProvider
             key={index}
@@ -28,7 +29,7 @@ const Skills = () => {
             index={index}
           />
         ))}
-      </div>
+      </div> */}
 
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Frontend_skill.map((image, index) => (
@@ -53,7 +54,7 @@ const Skills = () => {
         ))}
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Full_stack.map((image, index) => (
+        {Css_skill.map((image, index) => (
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -64,7 +65,7 @@ const Skills = () => {
         ))}
       </div>
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Other_skill.map((image, index) => (
+        {Full_stack.map((image, index) => (
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -74,6 +75,17 @@ const Skills = () => {
           />
         ))}
       </div>
+      {/* <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Other_skill.map((image, index) => (
+          <SkillDataProvider
+            key={index}
+            src={image.Image}
+            width={image.width}
+            height={image.height}
+            index={index}
+          />
+        ))}
+      </div> */}
 
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
--- app/constants/index.ts
+++ app/constants/index.ts
@@ -114,20 +114,32 @@ export const Frontend_skill = [
     height: 65,
   },
   {
-    skill_name: "Tailwind Css",
-    Image: "/tailwind.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Material UI",
-    Image: "/mui.png",
+    skill_name: "Type Script",
+    Image: "/ts.png",
     width: 80,
     height: 80,
   },
   {
     skill_name: "React",
     Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Vue",
+    Image: "/vue.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next js 14",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Nuxt js",
+    Image: "/nuxt.png",
     width: 80,
     height: 80,
   },
@@ -144,20 +156,65 @@ export const Frontend_skill = [
     height: 80,
   },
   {
-    skill_name: "Type Script",
-    Image: "/ts.png",
+    skill_name: "GatsbyJS",
+    Image: "/gatsby.png",
     width: 80,
     height: 80,
   },
   {
-    skill_name: "Next js 13",
-    Image: "/next.png",
+    skill_name: "Webflow",
+    Image: "/webflow.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Vite",
+    Image: "/vite.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Css_skill = [
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    Image: "/mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Bootstrap",
+    Image: "/bootstrap.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "sass",
+    Image: "/sass.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Vuetify",
+    Image: "/vuetify.png",
     width: 80,
     height: 80,
   },
 ];
 
 export const Backend_skill = [
+  {
+    skill_name: "Python",
+    Image: "/python.png",
+    width: 80,
+    height: 80,
+  },
   {
     skill_name: "Node js",
     Image: "/node-js.png",
@@ -171,10 +228,22 @@ export const Backend_skill = [
     height: 80,
   },
   {
+    skill_name: "FastAPI",
+    Image: "/fastapi.png",
+    width: 80,
+    height: 80,
+  },
+  {
     skill_name: "Mongo db",
     Image: "/mongodb.png",
     width: 40,
     height: 40,
+  },
+  {
+    skill_name: "DynanmoDB",
+    Image: "/dynamodb.png",
+    width: 60,
+    height: 60,
   },
   {
     skill_name: "Fire base",
@@ -206,41 +275,45 @@ export const Backend_skill = [
     width: 80,
     height: 80,
   },
+  {
+    skill_name: "Django",
+    Image: "/django.png",
+    width: 80,
+    height: 80,
+  },
 ];
 
 export const Full_stack = [
-  {
-    skill_name: "React Native",
-    Image: "/ReactNative .png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Tauri",
-    Image: "/tauri.svg",
-    width: 70,
-    height: 70,
-  },
   {
     skill_name: "Docker",
     Image: "/docker.webp",
     width: 70,
     height: 70,
   },
-
   {
     skill_name: "Figma",
     Image: "/figma.png",
     width: 50,
     height: 50,
   },
-];
-
-export const Other_skill = [
   {
-    skill_name: "Go",
-    Image: "/go.png",
-    width: 60,
-    height: 60,
+    skill_name: "Github",
+    Image: "/github.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "AWS",
+    Image: "/aws.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Google Cloud",
+    Image: "/googlecloud.png",
+    width: 70,
+    height: 70,
   },
 ];
+
+export const Other_skill = [];
