b0ad971f9051c18007dbc11f4ab457f8772e4cb5
yutounun <geek.yuto@gmail.com>
2024-09-21 10:48:44 -0700

    fix build err

--- app/components/main/Footer.tsx
+++ app/components/main/Footer.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  RxDiscordLogo,
-  RxGithubLogo,
-  RxInstagramLogo,
-  RxTwitterLogo,
-  RxLinkedinLogo,
-} from "react-icons/rx";
+import { RxDiscordLogo, RxGithubLogo } from "react-icons/rx";
 
 import { FaYoutube } from "react-icons/fa";
 
